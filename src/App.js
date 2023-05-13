6bcfb62ff7284b43bd88bec96029a13dcfc00b2a
nigalini <premala.it@gmail.com>
2023-05-13 15:00:09 +0530

    r

--- src/App.js
+++ src/App.js
@@ -2,22 +2,50 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const drop=(ev)=>{
+    ev.preventDefault();
+  var data = ev.dataTransfer.getData("text");
+  ev.target.appendChild(document.getElementById(data));
+  }
+  const allowDrop=(ev)=>{
+    ev.preventDefault();
+  }
+
+  const drag=(ev)=>{
+    ev.dataTransfer.setData("text", ev.target.id);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Drag and Drop</h2>
+
+<div id="div1" onDrop={drop} onDragOver={allowDrop}>
+  <div draggable="true" onDragStart={drag} id="drag1">
+    <h3> NAME 1</h3>
+    <h5> Designation 1</h5>
+      </div>
+      <div draggable="true" onDragStart={drag} id="drag2">
+    <h3> NAME 2</h3>
+    <h5> Designation 2</h5>
+      </div>
+      <div draggable="true" onDragStart={drag} id="drag3">
+    <h3> NAME 3</h3>
+    <h5> Designation 1</h5>
+      </div>
+      <div draggable="true" onDragStart={drag} id="drag4">
+    <h3> NAME 4</h3>
+    <h5> Designation 2</h5>
+      </div>
+      <div draggable="true" onDragStart={drag} id="drag5">
+    <h3> NAME 5</h3>
+    <h5> Designation 1</h5>
+      </div>
+    
+ 
+  
+</div>
+
+<div id="div2" onDrop={drop}  onDragOver={allowDrop}></div>
+<div id="div3" onDrop={drop} onDragOver={allowDrop}></div>
     </div>
   );
 }
